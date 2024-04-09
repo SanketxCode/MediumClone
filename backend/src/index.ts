dec781ff2aa2d888abfdb4dcc9abafc9f5525454
Sanket-777 <Ksanket114402@gmail.com>
2024-04-09 15:55:33 +0530

    added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,17 @@ import { Hono } from "hono";
 
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
+
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     SECRET: string;
   };
 }>();
-
-app.route("/api/v1/user",userRouter);
-app.route("/api/v1/blog",blogRouter);
+app.use("*", cors());
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 //middleware to verify user using jwt token
 
 export default app;
