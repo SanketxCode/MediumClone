5d37ced959871199dd3d6da56b698d942cb3fcd6
Sanket-777 <Ksanket114402@gmail.com>
2024-04-09 13:18:47 +0530

    signin ui done

--- frontend/src/components/SignInBox.tsx
+++ frontend/src/components/SignInBox.tsx
@@ -3,38 +3,34 @@ import { InputBox } from "./InputBox"
 import { Button } from "./Button"
 import { useState } from "react"
 
-export const SignUpbox = () => {
+export const SignInbox = () => {
 
     const [showPassword, setShowPassword] = useState(false);
     return <>
 
-        <div className=" h-screen flex justify-center flex-col bg-backg items-center ">
-            <div className="text-4xl font-extrabold ">
-                Welcome to Medium
+        <div className=" h-screen flex justify-center flex-col bg-backg items-center  ">
+            <div className="text-4xl font-extrabold text-center ">
+                Welcome Back to Medium
             </div>
-            <div className=" p-6  m-3 rounded-lg w-fit	 bg-white shadow-2xl">
+            <div className=" p-6  mt-4 rounded-lg  bg-white shadow-2xl">
                 <div className="flex justify-center text-center">
                     <div className="  ">
+
                         <div className="text-3xl font-extrabold ">
-                            Create an Account
+                            Login
                         </div>
                         <div className="text-slate-400">
-                            Already have an account ?
-                            <Link className="underline pl-2" to={'/signin'}>
-                                Login
+                            Don't have an account ?
+                            <Link className="underline pl-2" to={'/signup'}>
+                                Signup
                             </Link>
                         </div>
-                        <InputBox label="Username" type="text" placeholder="Enter name here" onChange={(e) => {
 
-                        }} />
                         <InputBox label="Email" type="text" placeholder="Enter email here" onChange={(e) => {
 
                         }} />
 
                         <InputBox label="Password" type={showPassword ? "text" : "password"} placeholder="Enter password here" onChange={(e) => {
-
-                        }} />
-                        <InputBox label="Confirm Password" type={showPassword ? "text" : "password"} placeholder="Enter password again" onChange={(e) => {
 
                         }} />
 
@@ -43,7 +39,7 @@ export const SignUpbox = () => {
                             <label htmlFor="default-checkbox" className="mt-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Show Password </label>
                         </div>
 
-                        <Button label="Sign Up" />
+                        <Button label="Sign In" />
                     </div>
                 </div>
 
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,6 +1,13 @@
-
+import { SignInbox } from "../components/SignInBox"
+import { Quote } from "../components/Quote"
 export const Signin = () =>{
     return <>
-    
+     <div className="lg:grid grid-cols-2">
+            <SignInbox />
+            <div className="visible lg:visible">
+            <Quote />
+            </div>
+        </div>
+
     </>
 }
