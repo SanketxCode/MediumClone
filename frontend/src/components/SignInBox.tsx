9b3b7f2093adfcb38e75d2865b8c7802e60d2528
Sanket-777 <Ksanket114402@gmail.com>
2024-04-11 22:55:02 +0530

    added spinner

--- frontend/src/components/SignInBox.tsx
+++ frontend/src/components/SignInBox.tsx
@@ -5,21 +5,24 @@ import { useState } from "react"
 import { signIninput } from "@sanket-777/medium-blog-common"
 import axios from "axios"
 import { BACKEND_URL } from "../config"
+import { Spinner } from "./Spinner"
 export const SignInbox = () => {
 
     const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setloading] = useState(false);
     const [postInputs, setPostinputs] = useState<signIninput>({
         username: "",
         password: ""
     })
     async function handleclick() {
         try {
+            setloading(true);
             const response = await axios.post(`${BACKEND_URL}/api/v1/user/signin`, postInputs);
             const jwt = "Bearer " + response.data.jwt;
             localStorage.setItem("token", jwt)
             localStorage.setItem("id", response.data.id)
-            alert(response.data.jwt)
+            setloading(false);
             navigate("/blogs");
         }
         catch (error) {
@@ -27,50 +30,53 @@ export const SignInbox = () => {
 
         }
     }
+
     return <>
-        <div className=" h-screen flex justify-center flex-col bg-backg items-center  ">
-            <div className="text-4xl font-extrabold text-center font-serif ">
-                Welcome Back to Medium
-            </div>
-            <div className=" px-6 py-4 p mt-4 rounded-lg  bg-white shadow-2xl">
-                <div className="flex justify-center text-center">
-                    <div className="  ">
 
-                        <div className="text-3xl font-extrabold ">
-                            Login
-                        </div>
-                        <div className="text-slate-400">
-                            Don't have an account ?
-                            <Link className="underline pl-2" to={'/signup'}>
-                                Signup
-                            </Link>
-                        </div>
-
-                        <InputBox label="Email" type="text" placeholder="Enter email here" onChange={(e) => {
-                            setPostinputs(c => ({
-                                ...c,
-                                username: e.target.value
-                            }))
-                        }} />
-
-                        <InputBox label="Password" type={showPassword ? "text" : "password"} placeholder="Enter password here" onChange={(e) => {
-                            setPostinputs(c => ({
-                                ...c,
-                                password: e.target.value
-                            }))
-                        }} />
-
-                        <div className="flex items-center mb-4">
-                            <input id="default-checkbox" onChange={(e) => setShowPassword(e.target.checked)} type="checkbox" value="" className="mt-4 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-md focus:ring-blue-500  dark:bg-gray-700 dark:border-gray-600" />
-                            <label htmlFor="default-checkbox" className="mt-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Show Password </label>
-                        </div>
-
-                        <Button label="Sign In" handleclick={handleclick} />
-                    </div>
+        {loading && <Spinner />}
+            <div className=" h-screen flex justify-center flex-col bg-backg items-center  ">
+                <div className="text-4xl font-extrabold text-center font-serif ">
+                    Welcome Back to Medium
                 </div>
+                <div className=" px-6 py-4 p mt-4 rounded-lg  bg-white shadow-2xl">
+                    <div className="flex justify-center text-center">
+                        <div className="  ">
 
+                            <div className="text-3xl font-extrabold ">
+                                Login
+                            </div>
+                            <div className="text-slate-400">
+                                Don't have an account ?
+                                <Link className="underline pl-2" to={'/signup'}>
+                                    Signup
+                                </Link>
+                            </div>
+
+                            <InputBox label="Email" type="text" placeholder="Enter email here" onChange={(e) => {
+                                setPostinputs(c => ({
+                                    ...c,
+                                    username: e.target.value
+                                }))
+                            }} />
+
+                            <InputBox label="Password" type={showPassword ? "text" : "password"} placeholder="Enter password here" onChange={(e) => {
+                                setPostinputs(c => ({
+                                    ...c,
+                                    password: e.target.value
+                                }))
+                            }} />
+
+                            <div className="flex items-center mb-4">
+                                <input id="default-checkbox" onChange={(e) => setShowPassword(e.target.checked)} type="checkbox" value="" className="mt-4 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-md focus:ring-blue-500  dark:bg-gray-700 dark:border-gray-600" />
+                                <label htmlFor="default-checkbox" className="mt-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Show Password </label>
+                            </div>
+
+                            <Button label="Sign In" handleclick={handleclick} />
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
 
     </>
 }
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,11 +1,13 @@
 import { SignInbox } from "../components/SignInBox"
 import { Quote } from "../components/Quote"
+import { Spinner } from "../components/Spinner"
 export const Signin = () => {
+
     return <>
         <div className="lg:grid grid-cols-2 ">
             <SignInbox />
             <div className="hidden lg:block">
-                <Quote />
+             <Quote />
             </div>
         </div>
 
